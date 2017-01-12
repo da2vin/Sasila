18a3c06cc70c7e50e5372f6903ae3f10925482f0
DarkSand <darkwings_love@163.com>
2017-01-12 17:52:45 +0800

    SmartSpider

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+from PyQt4 import QtGui
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+app = QtGui.QApplication(sys.argv)
+widget = QtGui.QWidget()
+widget.resize(250, 150)
+widget.setWindowTitle('PyQt')
+widget.show()
+sys.exit(app.exec_())
