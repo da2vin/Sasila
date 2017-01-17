d0b1b99cc25d3257b77ca1146c7346bc6a5a25b5
DarkSand <darkwings_love@163.com>
2017-01-17 16:40:29 +0800

    SmartSpider

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from manager.spider_manager import SpiderManager
+import json
 
 app = Flask(__name__)
 manager = SpiderManager()
@@ -15,42 +16,42 @@ def hello_world():
 
 @app.route('/all')
 def get_all_spider(self):
-    pass
+    return json.dumps(manager.get_all_spider())
 
 
 @app.route('/find')
 def find_spider(self, spider_id):
-    pass
+    return json.dumps(manager.find_spider(spider_id))
 
 
 @app.route('/start')
 def start_spider(self, spider_id):
-    pass
+    return json.dumps(manager.start_spider(spider_id))
 
 
 @app.route('/restart')
 def restart_spider(self, spider_id):
-    pass
+    return json.dumps(manager.restart_spider(spider_id))
 
 
 @app.route('/stop')
 def stop_spider(self, spider_id):
-    pass
+    return json.dumps(manager.stop_spider(spider_id))
 
 
 @app.route('/pause')
 def pause_spider(self, spider_id):
-    pass
+    return json.dumps(manager.pause_spider(spider_id))
 
 
 @app.route('/detail')
 def get_spider_detail(self, spider_id):
-    pass
+    return json.dumps(manager.get_spider_detail(spider_id))
 
 
 @app.route('/init')
 def init_system(self):
-    pass
+    return json.dumps(manager.init_system())
 
 
 if __name__ == '__main__':
