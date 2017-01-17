be8931bcd2ef39a655eb81be2d8e5130ba748e44
DarkSand <darkwings_love@163.com>
2017-01-17 16:10:58 +0800

    SmartSpider

--- manager/spider_manager.py
+++ manager/spider_manager.py
@@ -7,4 +7,26 @@ sys.setdefaultencoding('utf-8')
 
 
 class SpiderManager(object):
-    pass
+    def __init__(self):
+        pass
+
+    def get_all_spider(self):
+        pass
+
+    def find_spider(self, spider_id):
+        pass
+
+    def start_spider(self, spider_id):
+        pass
+
+    def restart_spider(self, spider_id):
+        pass
+
+    def stop_spider(self, spider_id):
+        pass
+
+    def pause_spider(self, spider_id):
+        pass
+
+    def get_spider_detail(self, spider_id):
+        pass
