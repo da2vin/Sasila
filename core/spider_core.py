26d67883713d46f630c7c618c5979e8501154426
DarkSand <darkwings_love@163.com>
2017-01-17 15:28:10 +0800

    SmartSpider

--- core/spider_core.py
+++ core/spider_core.py
@@ -9,6 +9,16 @@ sys.setdefaultencoding('utf-8')
 class SpiderCore(object):
     def __init__(self):
         self.downloader = None
+        self.processor = None
+        self.scheduler = None
+
+    def create(self, processor):
+        self.processor = processor
+        return self
+
+    def set_scheduler(self, scheduler):
+        self.scheduler = scheduler
+        return self
 
     def set_downloader(self, downloader):
         self.downloader = downloader
