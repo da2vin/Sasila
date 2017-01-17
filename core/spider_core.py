d22bfd32de50c4e5f277e503048a626a1d2cf92b
DarkSand <darkwings_love@163.com>
2017-01-17 15:38:54 +0800

    SmartSpider

--- core/spider_core.py
+++ core/spider_core.py
@@ -11,6 +11,15 @@ class SpiderCore(object):
         self.downloader = None
         self.processor = None
         self.scheduler = None
+        self.pipline = None
+        self.spider_name = None
+        self.spider_id = None
+
+    def set_spider_name(self, spider_name):
+        self.spider_name = spider_name
+
+    def set_spider_id(self, spider_id):
+        self.spider_id = spider_id
 
     def create(self, processor):
         self.processor = processor
@@ -23,3 +32,6 @@ class SpiderCore(object):
     def set_downloader(self, downloader):
         self.downloader = downloader
         return self
+
+    def set_pipline(self, pipline):
+        self.pipline = pipline
---
+++ pipline/base_pipline.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class BasePipline(object):
+    pass
