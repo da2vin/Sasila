1831cc413b06502665ac4f4510b669b145a6ab86
DarkSand <darkwings_love@163.com>
2017-01-24 10:53:45 +0800

    SmartSpider

--- downloader/spider_request.py
+++ downloader/spider_request.py
@@ -8,9 +8,10 @@ sys.setdefaultencoding('utf-8')
 
 
 class Request(object):
-    def __init__(self, url=None, headers=None, method=None, cookies=None, meta=None):
+    def __init__(self, url=None, headers=None, method=None, cookies=None, meta=None, callback=None):
         self.url = url
         self.headers = headers
         self.method = method
         self.meta = meta
         self.cookies = cookies
+        self.callback = callback
