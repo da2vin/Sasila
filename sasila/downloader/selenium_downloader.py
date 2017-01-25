800a5f5bf0fbe87234f59ff9b3a59e6964c58512
DarkSand <darkwings_love@163.com>
2017-01-25 11:17:08 +0800

    sasila

--- sasila/downloader/selenium_downloader.py
+++ sasila/downloader/selenium_downloader.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
-
-from selenium import webdriver
-
 from sasila.downloader.base_downloder import BaseDownLoader
+from sasila.downloader.web_driver_pool import get_web_driver_pool
+from sasila import setting
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
 class SeleniumDownLoader(BaseDownLoader):
+    def __init__(self):
+        self.web_driver_pool = get_web_driver_pool(setting.DRIVER_POOL_SIZE)
+
     def download(self, requests):
-        web = webdriver.PhantomJS()
+        web = self.web_driver_pool.get()
         web.get(requests)
         return web.execute_script("return document.documentElement.outerHTML")
