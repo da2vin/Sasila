f4b7482de9367be507363abc627a0f80f13325cd
DarkSand <darkwings_love@163.com>
2017-01-25 11:45:05 +0800

    sasila

--- sasila/downloader/spider_request.py
+++ sasila/downloader/spider_request.py
@@ -7,7 +7,7 @@ sys.setdefaultencoding('utf-8')
 
 
 class Request(object):
-    def __init__(self, url=None, headers=None, method=None, cookies=None, meta=None, callback=None):
+    def __init__(self, url=None, headers=None, method=None, cookies=None, meta={}, callback=None):
         self.url = url
         self.headers = headers
         self.method = method
