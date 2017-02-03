e6e4f51ad46c2790336c64f095ccf47fd2477510
DarkSand <darkwings_love@163.com>
2017-02-03 16:29:02 +0800

    sasila

--- sasila/run.py
+++ sasila/run.py
@@ -15,42 +15,42 @@ def hello_world():
 
 
 @app.route('/all')
-def get_all_spider(self):
+def get_all_spider():
     return json.dumps(manager.get_all_spider())
 
 
 @app.route('/find')
-def find_spider(self, spider_id):
+def find_spider(spider_id):
     return json.dumps(manager.find_spider(spider_id))
 
 
 @app.route('/start')
-def start_spider(self, spider_id):
+def start_spider(spider_id):
     return json.dumps(manager.start_spider(spider_id))
 
 
 @app.route('/restart')
-def restart_spider(self, spider_id):
+def restart_spider(spider_id):
     return json.dumps(manager.restart_spider(spider_id))
 
 
 @app.route('/stop')
-def stop_spider(self, spider_id):
+def stop_spider(spider_id):
     return json.dumps(manager.stop_spider(spider_id))
 
 
 @app.route('/pause')
-def pause_spider(self, spider_id):
+def pause_spider(spider_id):
     return json.dumps(manager.pause_spider(spider_id))
 
 
 @app.route('/detail')
-def get_spider_detail(self, spider_id):
+def get_spider_detail(spider_id):
     return json.dumps(manager.get_spider_detail(spider_id))
 
 
 @app.route('/init')
-def init_system(self):
+def init_system():
     return json.dumps(manager.init_system())
 
 
