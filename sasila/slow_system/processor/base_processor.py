99825804071426b06c38372f31759e8445d3173d
DarkSand <darkwings_love@163.com>
2017-02-14 13:48:17 +0800

    sasila

--- sasila/slow_system/processor/base_processor.py
+++ sasila/slow_system/processor/base_processor.py
@@ -1,15 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import re
+from sasila.slow_system.downloader.http.spider_request import Request
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+
+def identity(x):
+    return x
+
+
+class Rule(object):
+    def __init__(self, link_extractor, callback=None, process_request=identity, priority=0):
+        self.link_extractor = link_extractor
+        self.callback = callback
+        self.process_request = process_request
+        self.priority = priority
+
+
+class LinkExtractor(object):
+    def __init__(self, regex_str=None):
+        self.regex = re.compile(regex_str)
+
+    def extract_links(self, response):
+        return [response.nice_join(link) for link in self.regex.findall(response.m_response.content)]
 
 
 class BaseProcessor(object):
     spider_id = None
     spider_name = None
     start_requests = []
+    rules = ()
+    allowed_domains = []
 
     def process(self, response):
-        raise NotImplementedError
+        if hasattr(self, 'rules'):
+            rules = getattr(self, 'rules', None)
+        else:
+            rules = ()
+        for rule in rules:
+            links = rule.link_extractor.extract_links(response)
+            for link in links:
+                request = Request(url=link, callback=rule.callback, priority=rule.priority)
+                request = rule.process_request(request)
+                yield request
---
+++ sasila/slow_system/processor/mzitu_proccessor2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+from sasila.slow_system.core.request_spider import RequestSpider
+from sasila.slow_system.pipeline.pic_pipeline import PicPipeline
+
+from base_processor import BaseProcessor, Rule, LinkExtractor
+from sasila.slow_system.downloader.http.spider_request import Request
+import os
+import uuid
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class MezituProcessor(BaseProcessor):
+    spider_id = 'mzitu'
+    spider_name = 'mzitu'
+    start_requests = [Request(url='http://www.mzitu.com/xinggan')]
+
+    rules = (
+        Rule(LinkExtractor(regex_str="http\://i\.meizitu\.net/\d{4}/\d{2}/[0-9a-z]+\.jpg"),
+             callback="save", priority=3),
+        Rule(LinkExtractor(regex_str="http\://www\.mzitu\.com/\d+"), priority=1),
+        Rule(LinkExtractor(regex_str="http\://www\.mzitu\.com/\d+/\d+"), priority=2),
+        Rule(LinkExtractor(regex_str="http\://www\.mzitu\.com/xinggan/page/\d+"), priority=0),
+    )
+
+    def save(self, response):
+        if response.m_response.content:
+            if not os.path.exists("img"):
+                os.mkdir("img")
+            with open("img/" + str(uuid.uuid1()) + ".jpg", 'wb') as fs:
+                fs.write(response.m_response.content)
+                print("download success!")
+
+
+if __name__ == '__main__':
+    spider = RequestSpider(MezituProcessor()).set_pipeline(PicPipeline()).start()
