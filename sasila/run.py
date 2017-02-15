df498e78cfc1c97eb64596072164b20e37a8efd2
DarkSand <darkwings_love@163.com>
2017-02-15 13:17:38 +0800

    sasila

--- sasila/run.py
+++ sasila/run.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from immediately_system.blueprints.jd import im_jd
-from slow_system.blueprints.slow_spider import slow_spider
+from slow_system.blueprints.slow_spiders import slow_spider
 
 app = Flask(__name__)
 app.register_blueprint(im_jd, url_prefix='/im/jd')
