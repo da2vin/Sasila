9422c340ac5e1ed5400c56dff15790db4bd034e9
DarkSand <darkwings_love@163.com>
2017-02-15 12:50:15 +0800

    sasila

--- sasila/slow_system/utils/httpobj.py
+++ sasila/slow_system/utils/httpobj.py
@@ -12,9 +12,6 @@ _urlparse_cache = weakref.WeakKeyDictionary()
 
 
 def urlparse_cached(request_or_response):
-    """Return urlparse.urlparse caching the result, where the argument can be a
-    Request or Response object
-    """
     if request_or_response not in _urlparse_cache:
         _urlparse_cache[request_or_response] = urlparse(request_or_response.url)
     return _urlparse_cache[request_or_response]
