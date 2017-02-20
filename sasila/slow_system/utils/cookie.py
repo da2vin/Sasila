1a9514caadb2ec49d3b2a8e8e0e0006b1469753b
DarkSand <darkwings_love@163.com>
2017-02-20 16:38:51 +0800

    sasila

--- sasila/slow_system/utils/cookie.py
+++ sasila/slow_system/utils/cookie.py
@@ -12,3 +12,9 @@ def formart_selenium_cookies(cookies):
     for c in cookies:
         cookie_dict[c['name']] = c['value']
     return json.dumps(cookie_dict).decode('unicode-escape')
+
+
+def formart_requests_cookies(cookies):
+    cookie_list = [{'name': c[0], 'value': c[1], 'path': '/', 'domain': '.jd.com'} for c in
+                   dict(json.loads(cookies)).items()]
+    return cookie_list
