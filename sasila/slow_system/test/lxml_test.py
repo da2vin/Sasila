5ac1957132bb31b9e7b71f8bf5483e7054bdeddc
DarkSand <darkwings_love@163.com>
2017-03-03 18:01:37 +0800

    sasila

--- sasila/slow_system/test/lxml_test.py
+++ sasila/slow_system/test/lxml_test.py
@@ -7,16 +7,43 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 text = '''
-<div>
-    <ul>
-         <li class="item-0"><a href="link1.html">first item</a></li>
-         <li class="item-1"><a href="link2.html">second item</a></li>
-         <li class="item-inactive"><a href="link3.html">third item</a></li>
-         <li class="item-1"><a href="link4.html">fourth item</a></li>
-         <li class="item-0"><a href="link5.html">fifth item</a>
-     </ul>
- </div>
+    <bookstore>
+
+    <book category="COOKING">
+      <title lang="en">Everyday Italian</title>
+      <author>Giada De Laurentiis</author>
+      <year>2005</year>
+      <price>30.00</price>
+    </book>
+
+    <book category="CHILDREN">
+      <title lang="en">Harry Potter</title>
+      <author>J K. Rowling</author>
+      <year>2005</year>
+      <price>29.99</price>
+    </book>
+
+    <book category="WEB">
+      <title lang="en">XQuery Kick Start</title>
+      <author>James McGovern</author>
+      <author>Per Bothner</author>
+      <author>Kurt Cagle</author>
+      <author>James Linn</author>
+      <author>Vaidyanathan Nagarajan</author>
+      <year>2003</year>
+      <price>49.99</price>
+    </book>
+
+    <book category="WEB">
+      <title lang="en">Learning XML</title>
+      <author>Erik T. Ray</author>
+      <year>2003</year>
+      <price>39.95</price>
+    </book>
+
+    </bookstore>
 '''
 html = etree.HTML(text)
-result = html.xpath('//li[contains(@class,\'item-1\')]/preceding::*')[0]
-print etree.tostring(result)
+result = html.xpath('//bookstore/book[@category=\'WEB\' and contains(./author/text(),\'Ray\')]/title')
+for r in result:
+    print etree.tostring(r)
