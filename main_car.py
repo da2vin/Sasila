685233e7ca45f26af0a16ad882bc0585baae8237
DarkSand <darkwings_love@163.com>
2017-06-26 14:05:00 +0800

    sasila

--- main_car.py
+++ main_car.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# from sasila.slow_system.processor.car_processor import Car_Processor
-# from sasila.slow_system.core.request_spider import RequestSpider
-# from sasila.slow_system.pipeline.console_pipeline import ConsolePipeline
-# from sasila.slow_system.pipeline.text_pipeline import TextPipelineCar
+from sasila.slow_system.processor.car_processor import Car_Processor
+from sasila.slow_system.core.request_spider import RequestSpider
+from sasila.slow_system.pipeline.console_pipeline import ConsolePipeline
+from sasila.slow_system.pipeline.text_pipeline import TextPipelineCar
 
-# if __name__ == '__main__':
-#     spider = RequestSpider(Car_Processor()).set_pipeline(ConsolePipeline()).set_pipeline(
-#             TextPipelineCar()).start()
+if __name__ == '__main__':
+    spider = RequestSpider(Car_Processor()).set_pipeline(ConsolePipeline()).set_pipeline(
+            TextPipelineCar()).start()
