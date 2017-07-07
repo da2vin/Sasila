2b4d943372d4dc35fd6b9c34ccff84b3cd46af4f
DarkSand <darkwings_love@163.com>
2017-07-07 10:40:20 +0800

    sasila

--- sasila/__init__.py
+++ sasila/__init__.py
@@ -5,13 +5,11 @@ import settings
 from flask import Flask
 from slow_system.blueprints.slow_spiders import slow_spider
 
-# from immediately_system.blueprints.jd import im_jd
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
-# app.register_blueprint(im_jd, url_prefix='/im/jd')
 app.register_blueprint(slow_spider, url_prefix='/slow_spider')
 
 
