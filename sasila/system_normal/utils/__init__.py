4f015f69a35b565d28911f631ab7c1009da98cfd
DarkSand <darkwings_love@163.com>
2017-11-02 10:01:29 +0800

    fix bugs

--- sasila/system_normal/utils/__init__.py
+++ sasila/system_normal/utils/__init__.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 import sys
 import logging
+import os
 
 if sys.version_info < (3, 0):
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
+if not os.path.exists("log"):
+    os.mkdir("log")
 logger = logging.getLogger("SASILA")
 logger.setLevel(logging.DEBUG)
 # 建立一个filehandler来把日志记录在文件里，级别为debug以上
-fh = logging.FileHandler("SASILA.log")
+fh = logging.FileHandler("log/SASILA.log")
 fh.setLevel(logging.ERROR)
 # 建立一个streamhandler来把日志打在CMD窗口上，级别为error以上
 ch = logging.StreamHandler()
